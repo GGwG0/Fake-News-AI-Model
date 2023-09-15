fb9c406b62e5922b6f9bf46ecd55123647a8498d
sharan2619 <144356787+sharan2619@users.noreply.github.com>
2023-09-15 17:36:41 +0800

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -74,15 +74,11 @@ count_vectorizer = joblib.load('vectorizer(final).pkl')
 #     count_vectorizer = joblib.load('countvectorizer.pkl')
 
 
+if st.button("Predict"): 
 #Apply the preprocessing function to the 'text' column
 processed_article = preprocess(article)
-
 processed_articles = ' '.join(processed_article)
-
-
-
 new_article_vectorized_count = count_vectorizer.transform([processed_articles])
-
 prediction = model.predict(new_article_vectorized_count)
 
 if prediction[0] == 1:
